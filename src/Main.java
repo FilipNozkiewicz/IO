24904a93dcb53d78ccc4fba78b8c2d51b719a418
Filipnoz8888 <filas08@gmail.com>
2018-03-18 20:47:50 +0100

    saassd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+
+   public static void main(String[] args) throws IOException {
+       Read read = new Read();
+       read.read1("data.txt");
+       for(int i = 0 ; i  < read.ar.size (); i ++){
+           System.out.print(read.ar.get ( i ));
+       }
+       System.out.println ();
+       System.out.println(read.ar.size ());
+   }
 }
