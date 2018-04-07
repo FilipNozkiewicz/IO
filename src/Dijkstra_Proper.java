db6c9e68720d1e13e6a45e766be20e1b4309aa3c
mihaudob <mdobiecki97@gmail.com>
2018-04-07 16:54:30 +0200

    Analiza kodu 2.0 Nie łapie do końca jeszcze tylko Dijkstra_Proper

--- src/Dijkstra_Proper.java
+++ src/Dijkstra_Proper.java
@@ -11,36 +11,36 @@ public class Dijkstra_Proper {
 
     public void dijkstra (ArrayList<ArrayList<Integer>> matrix){
 
-        Integer dlugosc = matrix.size();
-        distance = new ArrayList<Integer> ( dlugosc );
-        visited = new ArrayList<Integer> ( dlugosc );
-        path = new ArrayList<Integer> ( dlugosc );
+        Integer size = matrix.size();
+        distance = new ArrayList<Integer> ( size );
+        visited = new ArrayList<Integer> ( size );
+        path = new ArrayList<Integer> ( size );
 
-        for(int i = 0 ; i < dlugosc ; i++) {
+        for(int i = 0 ; i < size ; i++) {
             visited.add ( i, 0 );
             path.add ( i, 0 );
             distance.add ( i, 0 );
         }
 
-      for(int i = 0 ; i < dlugosc ; i++){
-            for(int j = 0 ; j < dlugosc ; j++){
+      for(int i = 0 ; i < size ; i++){
+            for(int j = 0 ; j < size ; j++){
               if(matrix.get ( i ).get ( j ) ==  (0)){
                   matrix.get ( i ).set ( j , 999);
               }
           }
       }
-      for(int i = 0 ; i < dlugosc ; i++){
+      for(int i = 0 ; i < size ; i++){
             distance.set ( i , matrix.get ( 0 ).get ( i ) );
       }
 
 
       distance.set ( 0 , 0 );
-      visited.set ( 0,0 );
+      visited.set ( 0, 0 );
 
-      for(int i = 0 ; i < dlugosc ; i++){
+      for(int i = 0 ; i < size ; i++){
           min = 999;
 
-          for(int j = 0 ; j < dlugosc ; j++){
+          for(int j = 0 ; j < size ; j++){
               if(min > distance.get ( j ) && visited.get ( j ) != 1){
                   min = distance.get ( j );
                   NextNode = j;
@@ -49,7 +49,7 @@ public class Dijkstra_Proper {
           }
           visited.set(NextNode , 1);
 
-          for(int c = 0 ; c < dlugosc ; c++){
+          for(int c = 0 ; c < size ; c++){
               if(visited.get ( c ) != 1){
                   Integer comp = matrix.get( NextNode ).get( c );
                   int value = (int)(comp + min);
