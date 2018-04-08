942f79ce178964a0dd49a6073e147eed21695356
mihaudob <mdobiecki97@gmail.com>
2018-04-08 17:34:20 +0200

    Class Parcel skończona - stworze nową klasę teraz któa będzie je generować

--- src/Parcel.java
+++ src/Parcel.java
@@ -1,21 +1,38 @@
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Parcel {
-    public static ArrayList <Integer>  address = new ArrayList<Integer> (2);
-    //współrzędna x,y
-    int  driver; //numer kierowcy - po numerach będzie łatwiej niż po imionach?
-    Timestamp order = new Timestamp(System.currentTimeMillis());
+    public static ArrayList <Integer>  address = new ArrayList<Integer> (2); //współrzędna x,y
+    int  driver; //numer kierowcy (1 do N) - po numerach będzie łatwiej niż po imionach
+    Timestamp order = new Timestamp(System.currentTimeMillis()); //obecny czas
+    int numberOfDrivers = 5; //ilość kierowców która u nas pracuje
+    int coordinatesX = 50;
+    int coordinatesY = 50;
 
+    Random random = new Random();
 
-    public Parcel() {
-
+    public Parcel() { // randomowo tworzymy ze wszystkim
+        int x = random.nextInt(coordinatesX);
+        int y = random.nextInt(coordinatesX);
+        address.add(0, x);
+        address.add(1, y);
+        driver = random.nextInt(5) + 1;
     }
 
-    public Parcel(int x, int y, int driver) {
-        address.set(0, x);
-        address.set(1, y);
+    public Parcel(int x, int y, int driver) { // teraz dodam taką by deklarować kierowce
+        address.add(0, x);
+        address.add(1, y);
         this.driver = driver;
+    }
+    public Parcel(int x, int y ) { // teraz dodam taką by go losowa
+        address.add(0, x);
+        address.add(1, y);
+        driver = random.nextInt(numberOfDrivers) + 1;
+    }
+
+    public int getDriver() {
+        return driver;
     }
 
     public Timestamp getOrder() {
@@ -23,12 +40,12 @@ public class Parcel {
     }
 
     public static int getX() { // zwraca X
-     try {
-        return address.get(0);
-    }catch (Exception e){
-        System.out.println("Bład z współrzędnymi - zwracam X = 0");
-        return 0;
-    }
+        try {
+            return address.get(0);
+        }catch (Exception e){
+            System.out.println("Bład z współrzędnymi - zwracam X = 0");
+            return 0;
+        }
     }
     public static int getY() { //zwraca Y
         try {
@@ -39,13 +56,18 @@ public class Parcel {
         }
     }
 
-    public static void setAddress(ArrayList<Integer> address) {
-        Parcel.address = address;
+    public String getDescription(){
+        String coordinates = new String();
+        coordinates = getOrder() +
+                " Coordinates: ( " +
+                getX() + (" , ") + getY() + (" )") +
+                " Driver number: " + getDriver();
+        return coordinates;
     }
-    
+
     public static void main(String[] args) {
         Parcel parcel = new Parcel();
-        System.out.println(parcel.getX());
+        System.out.println(parcel.getDescription());
 
     }
 }
