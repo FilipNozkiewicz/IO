012bb94694693bca3756a7410955f203f643a829
mihaudob <mdobiecki97@gmail.com>
2018-04-14 23:30:47 +0200

    Jak wkładać kolejne rzeczy z ver i har do arraya?

--- src/AdjacentMatrixGenerator.java
+++ src/AdjacentMatrixGenerator.java
@@ -16,23 +16,23 @@ public class AdjacentMatrixGenerator {
                 adjacent.get(i).add(j, 0); // chce wypełnić zerami macierz sąsiedztwa NxN - działa
             }
         }
+
         for(int i = 0 ; i < numberOfNeighbors; i++ ){
             for(int j = 0 ; j < numberOfNeighbors ; j++ ){// nastepnie do elementow dodanej arraylisty dodajemy kolejne elementy
-                if(j == i && (j < numberOfNeighbors -2)){ // chce przesunąć j o jeden do przodu i wypełnić kolejnymi Horyzontalnymi
+                if(j == i && (j < numberOfNeighbors -1)){ // chce przesunąć j o jeden do przodu i wypełnić kolejnymi Horyzontalnymi
                     adjacent.get(i).set(j+1,2);
                 }
-                if(j == i && (i < numberOfNeighbors -2)){ // chce przesunąć i o jeden do przodu i wypełnić kolejnymi Vertykalnymi
+                if(j == i && (i < numberOfNeighbors -1)){ // chce przesunąć i o jeden do przodu i wypełnić kolejnymi Vertykalnymi
                     adjacent.get(i+1).set(j,2);
                 }
-                if(j == i && (j < numberOfNeighbors - jump -1)){ // chce przesunąć j o skok czyli cały rząd do przodu i wypełnić kolejnymi Horyzontalnymi
+                if(j == i && (j < numberOfNeighbors - jump )){ // chce przesunąć j o skok czyli cały rząd do przodu i wypełnić kolejnymi Horyzontalnymi
                     adjacent.get(i).set(j + jump,3);
                 }
-                if(j == i && (i < numberOfNeighbors - jump - 1)){ // chce przesunąć i o skok czyli cały rząd jeden do przodu i wypełnić kolejnymi Vertykalnymi
+                if(j == i && (i < numberOfNeighbors - jump )){ // chce przesunąć i o skok czyli cały rząd jeden do przodu i wypełnić kolejnymi Vertykalnymi
                     adjacent.get(i + jump).set(j,3);
                 }
             }
         }
-
     }
     public void print(ArrayList<ArrayList<Integer>> temp){
         for(ArrayList<Integer> x  : temp){  // dla kazeej arrraylisty w obiekcie ar
