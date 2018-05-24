ac9beb22fe2d090c69bb8dd9a718a2beff0251e1
mihaudob <mdobiecki97@gmail.com>
2018-05-24 09:10:25 +0200

    wypisywanie

--- src/WriteReport.java
+++ src/WriteReport.java
@@ -18,18 +18,10 @@ public class WriteReport {
         for(RouteGenerator rg : routeGenerators){
             writeReport.getAllRouteGenerators().add(rg);
         }
-        System.out.println("Wszystkie daty w pliku - return stringa i do txt");
         writeReport.makeTimeList();
-        System.out.println("Wszyscy kierowcy i ilość w pliku - return stringa i do txt");
+        writeReport.writeTimeToFile("report.txt");
         writeReport.makeDriversList();
-
-
-        try {
-            writeReport.writingStringToFile("report.txt","LOL");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writeReport.writeDriversToFile("report.txt");
     }
 
     public ArrayList<Driver> getDrivers() {
@@ -39,42 +31,64 @@ public class WriteReport {
     public ArrayList<RouteGenerator> getAllRouteGenerators() {
         return allRouteGenerators;
     }
+    public void writeDriversToFile(String fileName){
+        String stringDriver;
+        try {
+            writingStringToFile(fileName, "Wykaz kierowców: ");
+            for (Driver driver : drivers){
+                stringDriver = "";
+                stringDriver = driver.getName() + " ilość tras: " + driver.getHowManyTimesThereIs();
+                writingStringToFile(fileName, stringDriver);
+            }
+            writingStringToFile(fileName, " ");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeTimeToFile(String fileName){
+        try {
+            writingStringToFile(fileName, "Wszystkie daty: ");
+            for(String date : dates) {
+                    writingStringToFile(fileName, date);
+            }
+            writingStringToFile(fileName, " ");
+            }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void makeTimeList(){
         Timestamp timestamp = new Timestamp(allRouteGenerators.get(1).order);
         dates.add(timestamp.toString().substring(0,10));
-        for(RouteGenerator rg : allRouteGenerators){
+        for(RouteGenerator rg : allRouteGenerators) {
             timestamp = new Timestamp(rg.getOrder());
-            String date = timestamp.toString().substring(0,10);
-            for (String s : dates){
-                if(!s.equals(date)){
+            String date = timestamp.toString().substring(0, 10);
+            for (String s : dates) {
+                if (!s.equals(date)) {
                     dates.add(date);
                 }
 
             }
         }
-        for (String s : dates){
-            System.out.println(s);
-        }
     }
 
 
     public void makeDriversList(){
-            for(RouteGenerator rg : allRouteGenerators){
-                drivers.add(new Driver(rg.getDriverName(), 1));
-            }
-            for( int i = 0; i < drivers.size(); i++){
-                for(int j = i + 1; j< drivers.size() - 1; j++){
-                    if (drivers.get(i).getName().equals(drivers.get(j).getName())) {
-                        drivers.get(i).setHowManyTimesThereIs(drivers.get(i).getHowManyTimesThereIs()+1);
-                        drivers.remove(j);
-                    }
+        for(RouteGenerator rg : allRouteGenerators){
+            drivers.add(new Driver(rg.getDriverName(), 1));
+        }
+        for( int i = 0; i < drivers.size(); i++){
+            for(int j = i + 1; j< drivers.size() - 1; j++){
+                if (drivers.get(i).getName().equals(drivers.get(j).getName())) {
+                    drivers.get(i).setHowManyTimesThereIs(drivers.get(i).getHowManyTimesThereIs()+1);
+                    drivers.remove(j);
                 }
             }
-
-            for (Driver driver : drivers){
-                System.out.println(driver.getName() + "  " + driver.getHowManyTimesThereIs());
-            }
+        }
     }
+
     public  int meanCreator(ArrayList<Integer> temp){
         int sum = 0;
         for (Integer i : temp){
